67b77f39cae548e1014ba2796f6fd34b54992a0d
Emma Liu <emmaliuisareader@gmail.com>
2025-08-25 14:00:00 +0000

    add cnn_test.py

---
+++ cnn_test.py
@@ -0,0 +1,58 @@
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+import torchvision
+import torchvision.transforms as transforms
+
+# model = resnet18(weights=ResNet18_Weights.DEFAULT)
+# data = torch.rand(1, 3, 64, 64)
+# labels = torch.rand(1, 1000)
+
+# prediction = model(data)
+
+# loss = (prediction-labels).sum()
+# loss.backward()
+
+# optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+
+# optim.step()
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
